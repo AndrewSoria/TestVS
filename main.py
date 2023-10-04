be7a820f4023f1bc465c6afe689399698b956672
Soria <aasoria@cps.k12.il.us>
2023-10-04 08:52:59 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+#data abstraction when you need to simplify data fro something more complex
+list1=[] #is an example of data abstraction
+#instead of writing out every variable at a time, you can write them all at once with a list
+#data absrtaction is when you simplify data
